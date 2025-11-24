62d1bc64754813fc036c6b5d6b6165d7d1ab6ef3
IgorKulduaev <and.sobol39@mail.ru>
2025-11-24 13:53:52 +0200

    Add files via upload

---
+++ lab6_lib/ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+using System.Numerics;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int bewbebe(int[,] mas)
+        {
+            int Product = 1;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    if (mas[i, j] % 2 != 0)
+                    {
+                        Product *= mas[i, j];
+                    }
+                }
+            }
+            return Product;
+        }
+    }
+}
+
+
---
+++ lab6_lib/ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+try
+{
+    int[,] myMas = new int[8, 3];
+    Random rand=new Random();
+    for (int i = 0;i<myMas.GetLength(0);i++)
+    {
+        for (int j = 0;j<myMas.GetLength(1);j++)
+        {
+            myMas[i, j] = rand.Next(1, 10);
+            Console.Write(myMas[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Произведение нечетных элементов матрицы:{ClassLibrary1.Class1.bewbebe(myMas)}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
